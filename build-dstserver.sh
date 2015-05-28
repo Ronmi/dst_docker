404b8fbca1c95e14895a9afb7faa90138fb96033
Ronmi Ren <ronmi.ren@gmail.com>
2015-05-28 16:00:00 +0800

    fix mod disapear after update

--- build-dstserver.sh
+++ build-dstserver.sh
@@ -7,8 +7,8 @@ MAINTAINER Ronmi Ren <ronmi.ren@gmail.com>
 RUN useradd -d /home/dst -U -u $USER_ID -m dst
 COPY initialize.sh /initialize.sh
 COPY update.sh /home/dst/update.sh
+COPY dedicated_server_mods_setup.lua /home/dst/dedicated_server_mods_setup.lua
 RUN chown -R dst:dst /home/dst && /initialize.sh
-COPY dedicated_server_mods_setup.lua /home/dst/dstserver/mods/dedicated_server_mods_setup.lua
 USER $USER_ID
 WORKDIR /home/dst/dstserver/bin
 ENTRYPOINT /home/dst/dstserver/bin/dontstarve_dedicated_server_nullrenderer
--- dst/update.sh
+++ dst/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
 ./steamcmd.sh +login anonymous +force_install_dir /home/dst/dstserver +app_update 343050 validate +quit
+cp dedicated_server_mods_setup.lua dstserver/mods/
