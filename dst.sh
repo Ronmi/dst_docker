775ea426fdbab90082a366519dd2fff13f23df50
Ronmi Ren <ronmi.ren@gmail.com>
2015-06-11 12:14:57 +0800

    Auto detect privilege, use sudo if no proper privilege

--- dst.sh
+++ dst.sh
@@ -5,6 +5,14 @@
 MY_DIR="$(dirname "$(readlink -fn "$0")")"
 BASEDIR="${MY_DIR}/servers"
 DIRS=$(find "$BASEDIR" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
+SUDO=
+
+# detect if we have proper privilege to use docker
+docker ps > /dev/null 2>&1
+if [[ $? != 0 ]]
+then
+    SUDO=sudo
+fi
 
 function removeall() {
     for i in $DIRS
@@ -35,12 +43,12 @@ USER $USER_ID
 WORKDIR /home/dst/dstserver/bin
 " | tee "${MY_DIR}/dst/Dockerfile"
 
-    docker build -t dstserver "${MY_DIR}/dst"
+    $SUDO docker build -t dstserver "${MY_DIR}/dst"
 }
 
 function do_uninstall() {
     removeall
-    docker rmi -f dstserver
+    $SUDO docker rmi -f dstserver
 }
 
 function help() {
@@ -53,14 +61,14 @@ function help() {
 function start() {
     CONTAINER="dst_$1"
     log_action_begin_msg "Starting docker container $1"
-    docker start "$CONTAINER" > /dev/null 2>&1
+    $SUDO docker start "$CONTAINER" > /dev/null 2>&1
     log_action_end_msg $?
 }
 
 function stop() {
     CONTAINER="dst_$1"
     log_action_begin_msg "Stoping docker container $1"
-    docker stop "$CONTAINER" > /dev/null 2>&1
+    $SUDO docker stop "$CONTAINER" > /dev/null 2>&1
     log_action_end_msg $?
 }
 
@@ -73,7 +81,7 @@ function cls() {
 function remove() {
     CONTAINER="dst_$1"
     log_action_begin_msg "Removing docker container $1"
-    docker rm "$CONTAINER" > /dev/null 2>&1
+    $SUDO docker rm "$CONTAINER" > /dev/null 2>&1
     log_action_end_msg $?
 }
 
@@ -82,25 +90,25 @@ function create() {
     FN="$BASEDIR/$1/settings.ini"
     PORT=$(grep -F server_port "$FN" | sed 's/ //g' | cut -d '=' -f 2)
     log_action_begin_msg "Creating container $CONTAINER (port $PORT)"
-    docker create -v $BASEDIR/$1:/home/dst/.klei/DoNotStarveTogether --entrypoint /home/dst/start.sh --name $CONTAINER -p $PORT:$PORT/udp dstserver > /dev/null 2>&1
+    $SUDO docker create -v $BASEDIR/$1:/home/dst/.klei/DoNotStarveTogether --entrypoint /home/dst/start.sh --name $CONTAINER -p $PORT:$PORT/udp dstserver > /dev/null 2>&1
     log_action_end_msg $?
 }
 
 function update_baseimage() {
     log_action_begin_msg "Force removing dstserver_upgrade container"
-    docker rm -f dstserver_upgrade > /dev/null 2>&1
+    $SUDO docker rm -f dstserver_upgrade > /dev/null 2>&1
     log_action_end_msg
 
     log_action_begin_msg "Running update.sh"
-    docker run -it -u dst --name dstserver_upgrade --entrypoint /home/dst/update.sh dstserver > /dev/null 2>&1
+    $SUDO docker run -it -u dst --name dstserver_upgrade --entrypoint /home/dst/update.sh dstserver > /dev/null 2>&1
     log_action_end_msg $?
 
     log_action_begin_msg "Commit dstserver_upgrade to dstserver"
-    docker commit dstserver_upgrade dstserver > /dev/null
+    $SUDO docker commit dstserver_upgrade dstserver > /dev/null
     log_action_end_msg $?
 
     log_action_begin_msg "Remove dstserver_upgrade container"
-    docker rm dstserver_upgrade > /dev/null
+    $SUDO docker rm dstserver_upgrade > /dev/null
     log_action_end_msg $?
 }
 
